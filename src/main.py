d8f8ae2c1277413d5e1c48536e9629757ecb5d42
meierkilian <47787442+meierkilian@users.noreply.github.com>
2024-10-29 10:13:21 +0000

    Commented and cleaning

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ gen = MissionGenerator()
 templates = WpmlTemplate()
 
 gen.setPOI(51.42354723804961, -2.670840695536882, 48)
-wp = gen.generateTransect(90, [70, 100, 120], [100, 150, 200, 250])
+wp = gen.generateTransect([90, 110, 130], [30, 40, 50], [0, 20, 50, 100, 150])
 str = templates.getWaypointMission(wp, gen.getPOI())
-templates.generateMissionArchive("newCoolMission", str)
+templates.generateMissionArchive("fenswoodWpTest", str)
 
--- src/missionGenerator.py
+++ src/missionGenerator.py
@@ -6,36 +6,61 @@ class MissionGenerator:
         pass
 
     def setPOI(self, lat, lon, altEGM96):
+        """Set position of Point of Interest.
+
+        Args:
+            lat (float): POI latitude WGS84
+            lon (float): POI longitude WGS84
+            altEGM96 (float): POI absolute altitude EGM96
+        """        
         self.POI_lat = lat
         self.POI_lon = lon
         self.POI_altEGM96 = altEGM96
 
     def getPOI(self):
+        """Point of Interest getter.
+
+        Returns:
+            dict: dictionnary containing POI, field are "lat", "lon", "altAbs"
+        """        
         return {"lat": self.POI_lat, "lon": self.POI_lon, "altAbs": self.POI_altEGM96}
 
     def range2focalLength(slef, range):
         # some mapping between the two, maybe include the size of observed object
         return 30
 
-    def generateTransect(self, heading, setAltRel, setDist):
+    def generateTransect(self, setHeading, setAltRel, setDist):
+        """Generate waypoints for a set of transects mission.
+        The mission is a set of waypoints placed on the cylindrical grid described by setHeading, setAltRel and setDist.
+        At each waypoint, a picture is taken, looking at the POI.
+
+        Args:
+            setHeading (list): list of headings in degrees
+            setAltRel (list): list of altitudes relative to POI in meters
+            setDist (list): list of distances from the POI in meters
+
+        Returns:
+            list: waypoint list, can be passed to the wpmlTemplate to generate a DJI mission file
+        """        
         wp = []
         sind = lambda degrees: math.sin(math.radians(degrees))
         cosd = lambda degrees: math.cos(math.radians(degrees))
         
         # For all wapoints
-        for alt in setAltRel:
-            for dist in setDist:
-                # Compute absolute position from relative offsets
-                lat, lon, altAbs = pymap3d.enu2geodetic(dist * sind(heading), dist * cosd(heading), alt, self.POI_lat, self.POI_lon, self.POI_altEGM96)
+        for heading in setHeading:
+            for alt in setAltRel:
+                for dist in setDist:
+                    # Compute absolute position from relative offsets
+                    lat, lon, altAbs = pymap3d.enu2geodetic(dist * sind(heading), dist * cosd(heading), alt, self.POI_lat, self.POI_lon, self.POI_altEGM96)
 
-                # Compute set of gimbal positions
-                az, el, srange = pymap3d.geodetic2aer(self.POI_lat, self.POI_lon, self.POI_altEGM96, lat, lon, altAbs)
-                # Create waypoint
-                wp.append(
-                    {"lat": lat, "lon": lon, "altAbs": altAbs, "actions": [
-                        {"pitch": el, "yaw": az, "focalLength": self.range2focalLength(srange)}
-                ]}
-                )
+                    # Compute set of gimbal positions
+                    az, el, srange = pymap3d.geodetic2aer(self.POI_lat, self.POI_lon, self.POI_altEGM96, lat, lon, altAbs)
+                    # Create waypoint
+                    wp.append(
+                        {"lat": lat, "lon": lon, "altAbs": altAbs, "actions": [
+                            {"pitch": el, "yaw": az, "focalLength": self.range2focalLength(srange)}
+                    ]}
+                    )
         return wp
     
 if __name__ == "__main__":
